116c8bca1a78a431dd44e0bbe8bcd76863bd4aa3
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2023-06-02 16:02:29 +0200

    Update homecontroller
            public ApplicationDbContext _context { get; set; }
    
    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
            {
                _logger = logger;
                _context = context;
            }

--- Skolinlämning/Controllers/HomeController.cs
+++ Skolinlämning/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Skolinlämning.Data;
 using Skolinlämning.Models;
 using System.Diagnostics;
 
@@ -7,10 +8,12 @@ namespace Skolinlämning.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public ApplicationDbContext _context { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
